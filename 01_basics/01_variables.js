f1f5288e9a61fa5c0c16f9843718e59b6a399e60
radiantclasses155-cmyk <221337487+Jitesh-Yadav01@users.noreply.github.com>
2025-10-27 18:58:09 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,5 +8,11 @@ accountemail = "radiantclass155@gmail.com";
 accountPassword = "8977";
 accountCity = "Gurgaon";
 
+/*
+prefer not to use var
+use const by default
+use let if you want to reassign
+*/
+
 console.log(accountId);
 console.table([accountId, accountemail, accountPassword, accountCity]);
