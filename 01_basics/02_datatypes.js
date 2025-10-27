e9b90c0dd5849e6826fc18cd02e2086b57381658
radiantclasses155-cmyk <221337487+Jitesh-Yadav01@users.noreply.github.com>
2025-10-27 18:58:13 +0530

    Create 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // Enabling strict mode for better error checking
+
+// alert( 3 + 3 ) // we are using node js so alert will not work , we can usew it in browser
+
+let name = "Jitesh Yadav";
+let age = 18;
+let isLoggedIn = false;
+let state; // undefined
+let temperature = null;
+
+// number => 2 to power 53 
+// bigint => larger than 2 to power 53
+// string => "" , '' , ``
+// boolean => true , false
+// null => standalone value
+// undefined => variable without value
+// symbol => unique identifiers
