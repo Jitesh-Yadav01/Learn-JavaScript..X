9d8d0e09c95a0e6881d0672c7275650da2448bfb
radiantclasses155-cmyk <221337487+Jitesh-Yadav01@users.noreply.github.com>
2025-10-27 18:12:33 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 145566;
+let accountemail = "radiantclasses155@gmail.com";
+var accountPassword = "4564";
+ accountCity = "Rewari";
+
+// accountId = 4567; --- IGNORE ---
+accountemail = "radiantclass155@gmail.com";
+accountPassword = "8977";
+accountCity = "Gurgaon";
+
+console.log(accountId);
+console.table([accountId, accountemail, accountPassword, accountCity]);
